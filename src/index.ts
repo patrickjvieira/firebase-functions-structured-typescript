6b86e76b77c7259f027f16c7cebdd0ac5f8b1db4
Patrick <patrickjvieira@gmail.com>
2019-03-03 12:48:15 -0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+/**
+ * Search for '.ts' files recursively inside this directory 
+ * then to convert path of each file to a camel case name to be 
+ * registered as function name on firebase cloud function.
+ * 
+ * Based on:
+ *  https://codeburst.io/organizing-your-firebase-cloud-functions-67dc17b3b0da
+ *  https://github.com/firebase/functions-samples/issues/170
+ * 
+ * @author: Patrick Vieira
+ * @todo: Every function in each file must be exported by using 'exports = module.exports = function...'
+ * @todo: Admin SDK must be started only one time: 'try {admin.initializeApp(functions.config().firebase);} catch(e) {} '
+ */
+
+import * as glob from 'glob';
+import * as camelCase from 'camelcase';
+import * as path from 'path';
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp(functions.config().firebase);
+
+glob.sync('./**/*.js', { cwd: __dirname }).forEach((file)=> {
+    
+    const structuredName = camelCase(file.substring(2, file.length - path.extname(file).length).split('/').join('_')).trim();
+
+    if (structuredName !== path.basename(__filename).split('.')[0].trim()) {
+        module.exports[structuredName] = require(file);
+    }
+
+});
